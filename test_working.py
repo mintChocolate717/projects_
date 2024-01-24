750a10bf9a4a9a67a511e4d98ffa464dbaa294f3
Jiwoong Choi <jiwoongchoi0207@icloud.com>
2024-01-23 22:54:08 -0600

    Create test_working.py

---
+++ test_working.py
@@ -0,0 +1,21 @@
+from working import convert
+from pytest import raises
+
+
+def test_no_minutes():
+    assert convert("9 AM to 5 PM") == "09:00 to 17:00"
+    assert convert("10 PM to 8 AM") == "22:00 to 08:00"
+
+
+def test_normal():
+    assert convert("9:00 AM to 5:00 PM") == "09:00 to 17:00"
+    assert convert("10:30 PM to 8:50 AM") == "22:30 to 08:50"
+
+
+def test_invalid():
+    with raises(ValueError):
+        convert("9:60 AM to 5:60 PM")
+    with raises(ValueError):
+        convert("9 AM - 5 PM")
+    with raises(ValueError):
+        convert("09:00 AM - 17:00 PM")
